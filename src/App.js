21fa5974afef41799bd6852e9805a3337af7983a
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-14 18:40:39 -0700

    Adding search and add

--- src/App.js
+++ src/App.js
@@ -4,32 +4,35 @@ import './MovieList.js';
 
 
 
+//map 
+
+
+
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+    movies:  
+      [
+        {title: 'The Room'},
+        {title: 'The Neverending Story'},
+        {title: 'Willow'},
+        {title: 'Surf Ninjas'},
+        {title: 'Winston Checks In'},
+      ]
+    };
+  }
+
+
   render() {
-    let movies = 
-  [
-    {title: 'The Room'},
-    {title: 'The Neverending Story'},
-    {title: 'Willow'},
-    {title: 'Surf Ninjas'},
-    {title: 'Winston Checks In'},
-  ];
-
-  /*
-  Search function:
-  Use filter
-  Need to add state
-
-
-  */
-
-
-  const moviesList = movies.map((data) => <div class="movies" key={data.title}>{data.title}</div>)
     return (
-      <div class = "container">
-        <h1 class ="title">Movie List</h1>
-        {moviesList}
+      <div className = "container">
+        <h1 className ="title">Movie List</h1>
+          <input type="text" className="add" onChange={this.searchHandler}/>
+            {this.state.movies.map((item) => {
+              return <div className="movies" key={item.title}>{item.title}</div>
+            })
+            }
       </div>
     );
   }
